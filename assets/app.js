dda73145e3138be45d0e235fb91d05ef7d5f8388
Pavel Poronko <me@gibzzz.tk>
2021-09-29 23:44:12 +0300

    ADD: luxon date lib. FIX: createApp not defined

--- assets/app.js
+++ assets/app.js
@@ -3,17 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueCompositionAPI from "@vue/composition-api";
+import { createApp } from "@vue/composition-api";
 import './styles/app.css'
 import {FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText} from "@fortawesome/vue-fontawesome";
 
 Vue.use(VueCompositionAPI);
 
-Vue.component('FAIcon', FontAwesomeIcon)
-Vue.component('FALayers', FontAwesomeLayers)
-Vue.component('FALayersText', FontAwesomeLayersText)
+Vue.component('FAIcon', FontAwesomeIcon);
+Vue.component('FALayers', FontAwesomeLayers);
+Vue.component('FALayersText', FontAwesomeLayersText);
 
-const app = createApp(App)
-app.use(router)
-app.use(store)
-
+const app = createApp(App);
+app.use(router);
+app.use(store);
 app.mount("#app");
